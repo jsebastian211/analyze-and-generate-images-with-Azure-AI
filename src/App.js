7b908507b2a4d5180a8df3c091bdfa2831870bc8
Sebastian Gutierrez Daza <icsebastian.gutierrez@americanwell.com>
2023-11-05 18:43:01 -0500

    Add new IA functionalities

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import { analyzeImage } from './azure-image-analysis';
+import { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState("");
+  const [result, setResult] = useState("");
+  // Create a component that displays the text "Computer vision" in a <h1> tag
+  function setImage(e) {
+    setImageUrl(e);
+  }
+
+  function validateImage() {
+    analyzeImage(imageUrl).then(data => {
+      console.log(data);
+      setResult(  data);
+    });
+  }
+
+  return (
+    <div>
+      <h1>Computer Vision</h1>
+      <br></br>
+      <a>Insert URL or type prompt</a>
+      <br></br>
+      <input type="text" placeholder="Enter url to analize or textual prompt to generate an image" value={imageUrl} onChange={e => setImageUrl(e.target.value)}></input>
+      <br></br>
+      <button onClick={validateImage}>Analize</button>
+      <button>Generate</button>
+      <br></br>
+      <img src={imageUrl} alt="image" width="500" height="600"></img>
+      <pre>{JSON.stringify(result, null, 2)}</pre>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/azure-image-analysis.js
@@ -0,0 +1,23 @@
+
+
+
+export function analyzeImage(imageUrl) {
+  alert("1")
+  const endpoint = "https://instanciaprueba.cognitiveservices.azure.com/computervision/imageanalysis:analyze?api-version=2023-02-01-preview&features=tags,read,caption,denseCaptions,smartCrops,objects,people";
+  const API_KEY = process.env.API_KEY;
+  const params = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Ocp-Apim-Subscription-Key": API_KEY
+    },
+    body: JSON.stringify({ url: imageUrl })
+  };
+  return fetch(endpoint, params)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      return data;
+    })
+    .catch(error => console.log(error));
+}
