4f0d358a569b478a41566f5d6ac47b5a637d74f9
Sebastian Gutierrez Daza <icsebastian.gutierrez@americanwell.com>
2023-11-06 09:06:52 -0500

    Add new IA functionalities v16

--- src/azure-image-analysis.js
+++ src/azure-image-analysis.js
@@ -1,8 +1,9 @@
+
+
 export function analyzeImage(imageUrl) {
   alert("1")
-  
   const endpoint = "https://instanciaprueba.cognitiveservices.azure.com/computervision/imageanalysis:analyze?api-version=2023-02-01-preview&features=tags,read,caption,denseCaptions,smartCrops,objects,people";
-  const API_KEY =process.env.API_KEY ;
+  const API_KEY = `${process.env.REACT_APP_API_KEY}` ;
   console.log(API_KEY);
   const params = {
     method: "POST",
